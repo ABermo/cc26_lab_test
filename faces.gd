fe837a1100df36f188c92873317081d57865cbd2
Adam Bermingham <adambermingham1@gmail.com>
2026-04-13 12:43:25 +0100

    Now happy or sad

--- faces.gd
+++ faces.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 @onready var button = $Click_Me
-var active = false
-var number = 0
+var active = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
@@ -10,24 +10,42 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if button.pressed and not active:
-		active = true
-		await get_tree().create_timer(0.1).timeout
-		active = false
+	pass
 		
-		number = randi_range(1,10)
-		queue_redraw()
-
-
 func _draw():
-	gen_faces(number)
-	number = 0
+	gen_faces()
 
-func gen_faces(num):
-	for face in range(num):
-		var emotion = randi()%2
+func _on_button_pressed() -> void:
+	queue_redraw()
+	
+func gen_faces():
+	if button.pressed and active != 1:
+		active = 1
+	else:
+		active = 0
 		
-		var x = randi_range(0,1152)
-		var y = randi_range(0,648)
+	if active == 1:
+		var num = randi_range(1,10)
 		
-		draw_circle(Vector2(x,y), 100, Color.RED, false)
+		for face in range(num):
+			var emotion = randi()%2
+
+			var x = randi_range(0,1152)
+			var y = randi_range(0,648)
+			var r = randi_range(20,150)
+			
+			draw_circle(Vector2(x,y), r, Color.RED, false)
+			
+			y = y + (r/3)
+			var x1 = x - (r/2)
+			var x2 = x + (r/2)
+			
+			draw_line(Vector2(x1,y),Vector2(x2,y), Color.RED)
+			
+			if emotion == 0:
+				draw_line(Vector2(x1,y), Vector2((x1 - r/3), (y - r/3)), Color.RED)
+				draw_line(Vector2(x2,y), Vector2((x2 + r/3), (y - r/3)), Color.RED)
+			if emotion == 1:
+				draw_line(Vector2(x1,y), Vector2((x1 - r/3), (y + r/3)), Color.RED)
+				draw_line(Vector2(x2,y), Vector2((x2 + r/3), (y + r/3)), Color.RED)
+	active = 0
